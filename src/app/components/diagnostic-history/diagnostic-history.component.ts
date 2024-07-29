89b407d6e271b219a921d3ae53d32f2f217c0a44
manash-codes <manash.codes@gmail.com>
2024-07-29 23:49:58 +0530

    Update Diagnostic-Info
    Add assets

--- src/app/components/diagnostic-history/diagnostic-history.component.ts
+++ src/app/components/diagnostic-history/diagnostic-history.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-diagnostic-history',
@@ -6,24 +7,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./diagnostic-history.component.css']
 })
 export class DiagnosticHistoryComponent {
-  healthCards = [
-    {
-      title: "Respiratory Rate",
-      value: "20",
-      heartRate: "75",
-      image: "../../assets/icons/patients-info/Layer 8.png"
-    },
-    {
-      title: "Respiratory Rate",
-      value: "20",
-      heartRate: "75",
-      image: "../../assets/icons/patients-info/Layer 8.png"
-    },
-    {
-      title: "Respiratory Rate",
-      value: "20",
-      heartRate: "75",
-      image: "../../assets/icons/patients-info/Layer 8.png"
-    },
-  ]
+  canvas: any;
+  ctx: any;
+  chart: any;
+
+  @ViewChild('mychart') mychart: any;
+
+  ngAfterViewInit() {
+    this.canvas = this.mychart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.chart = new Chart(this.ctx, {
+      type: 'line',
+      data: {
+        labels: ['Oct, 2023', 'Nov, 2023', 'Dec, 2023', 'Jan, 2024', 'Feb, 2024', 'Mar, 2024'],
+        datasets: [
+          {
+            label: 'Sales: Company A',
+            data: [125, 173, 111, 128, 119, 160],
+            backgroundColor: [
+              'rgba(230, 111, 210, 1)',
+            ],
+            borderColor: [
+              'rgba(230, 111, 210, 1)',
+            ],
+            borderWidth: 2,
+            tension: 0.4,
+            pointBorderColor: 'white',
+            pointBorderWidth: 1,
+            pointRadius: 7,
+            pointHoverRadius: 8
+          },
+          {
+            label: 'Sales: Company A',
+            data: [103, 103, 91, 86, 73, 78],
+            backgroundColor: [
+              'rgba(140, 111, 230, 1)',
+            ],
+            borderColor: [
+              'rgba(140, 111, 230, 1)',
+            ],
+            borderWidth: 2,
+            tension: 0.3,
+            pointBorderColor: 'white',
+            pointBorderWidth: 1,
+            pointRadius: 7,
+            pointHoverRadius: 8
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false // Hide the legend
+          }
+        },
+        scales: {
+          y: {
+            type: 'linear',
+            min: 60,
+            max: 180,
+          },
+          x: {
+            grid: {
+              display: false
+            }
+          }
+        }
+      }
+    })
+  }
+
+
 }
